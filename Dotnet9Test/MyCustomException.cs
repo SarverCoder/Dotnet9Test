41c1bd2ec79460de04f59e9fe856cbd4a507070b
SarverCoder <sarvargamer@gmail.com>
2024-11-20 22:41:38 +0500

    Add project files.

---
+++ Dotnet9Test/MyCustomException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet9Test
+{
+    public class MyCustomException : Exception
+    {
+
+        public MyCustomException(string message) : base(message) 
+        {
+            
+        }
+
+        public MyCustomException(string message, Exception innerException) : base(message, innerException) 
+        {
+            
+        }
+
+    }
+}
---
+++ Dotnet9Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Dotnet9Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(IsValidName("Sarvar"));
+            Console.WriteLine(IsValidEmail("934548544"));
+
+            string pattern = @"^([A-Z]{1}[a-z]+)$";
+
+            Regex reg = new Regex(pattern);
+
+            Console.WriteLine(reg.IsMatch("Ali"));
+            
+        }  
+        
+        static bool IsValidName(string name)
+        {
+            string pattern = @"^([A-Z]{1}[a-z]+)$";
+
+            return Regex.IsMatch(name, pattern);
+        }
+
+        static bool IsValidEmail(string email)
+        {
+            string pattern = "(^[0-9]{9}$)|(^[0-9]{2}-[0-9]{3}-[0-9]{2}$)";
+
+            return Regex.IsMatch(email, pattern);
+           
+        }
+
+        
+    }
+}
